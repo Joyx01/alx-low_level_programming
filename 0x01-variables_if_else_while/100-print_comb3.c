bef86f36f90b498d3b210d826162723747a2cb5a
Joyx01 <obiefunadimz@gmail.com>
2022-09-09 21:46:16 -0700

    printing numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - print single digit numbers starting from 0
+*
+* Return: returns zero at the end
+*/
 int main(void)
 {
-int n;
+        int i, j;
 
-    return (0);
+        for (i = 0; i <= 9; i++)
+        {
+                for (j = 1; j <= 9; j++)
+                {
+                        if (j > i)                                                                        {
+                                putchar(i + '0');
+                                putchar(j + '0');
+                        if (i != 8)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
